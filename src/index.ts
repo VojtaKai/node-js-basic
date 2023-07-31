dc43cff4d75f41a6e6fe9e857d0bb7e0373760a2
Vojtech Kaiser <vksmail95@gmail.com>
2023-07-31 22:28:25 +0200

    connection to mongodb cluster

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from "express"
 import { router } from "./routes/login"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
 
-const app = Express()
+dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT ?? 3000
+
+mongoose
+    .connect(process.env.MONGODB_CLUSTER_URI as string)
+    .then(() => console.log("Connected to the DB"))
+    .catch(err => console.log("err", err))
 
 app.set("view engine", "ejs")
 
